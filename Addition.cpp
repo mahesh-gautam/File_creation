1a8f5c7e2181292b1dc1d14aa91e8aca30247f33
Mahesh_Mogulla24 <76099951+mahesh-gautam@users.noreply.github.com>
2023-07-01 11:59:25 +0530

    Add files via upload

---
+++ Addition.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	
+	
+	int a ,b,sum;
+	printf("Enter the values of a and  b\n");
+	scanf("%d %d",&a,&b);
+	
+	sum=a+b;
+	
+	printf("Sum of %d + %d = %d",a,b,sum);
+}
